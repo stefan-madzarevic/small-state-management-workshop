c48de042a1d64749599bd49cf8b7beb0ad414cf8
Stefan Madzarevic <stefan.madzarevic@carcodex.com>
2018-12-13 14:51:09 +0100

    1 - setState is asynchronous

--- src/App.js
+++ src/App.js
@@ -3,22 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = { count: 0};
+  increment = () => {
+    this.setState({count: this.state.count + 1});
+    this.setState({count: this.state.count + 1});
+    this.setState({count: this.state.count + 1});
+    console.log(this.state.count);
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+            <p>
+              Clicking on increment button will <code>console.log(this.state.count)</code> which will be 0.
+              <br/>That is happening because setState is asynchronous function.
+            </p>
+          <button className="pure-material-button-contained" onClick={this.increment}>Increment</button>
         </header>
       </div>
     );
